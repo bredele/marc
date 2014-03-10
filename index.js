22415924b3104f7951bc255f02fdecb57b1e5ee0
bredele <olivier.wietrich@gmail.com>
2014-03-09 18:45:05 -0600

    add test for dynamic markdown

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var Store = require('store-component'),
+		supplant = require('store-supplant'),
+		marked = require('marked');
+
 
 /**
  * Expose 'marc'
@@ -5,23 +9,28 @@
 
 module.exports = function(data) {
 
-  var store = data;
+  var store = new Store(data);
+  store.use(supplant);
 
 	/**
 	 * marc constructor.
 	 * @api public
 	 */
 
-	function marc() {
-
+	function marc(str, fn) {
+		var result = marked(str);
+		if(fn) {
+			result = store.supplant(result, fn);
+		}
+		return result;
 	}
 
 	marc.set = function(name, val) {
-
+		store.set(name, val);
 	};
 
 	marc.get = function(name) {
-
+		return store.get(name);
 	};
 
 	return marc;
